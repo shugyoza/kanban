f973ecd206df98443c5b2a16eea7e3de9573a7c0
Stephen Hanjaya <stephen.hanjaya@sonymusicpub.com>
2026-08-19 11:19:08 -0500

    updated kanban.go
    Signed-off-by: Stephen Hanjaya <stephen.hanjaya@sonymusicpub.com>

--- backend/internal/domain/kanban.go
+++ backend/internal/domain/kanban.go
@@ -1,1 +1,12 @@
 // business entities, aggregators & interface ports
+
+package domain
+
+import (
+	"context"
+)
+
+// KanbanUseCase defines the business rules available for the Kanban board.
+type KanbanUseCase interface {
+	GetBoardDetails(ctx context.Context, boardID string) (*BoardAggregate, error) // use * to pass a pointer to the BoardAggregate struct (instead of copied value), allowing for efficient memory usage and the ability to modify the original struct if needed.
+}

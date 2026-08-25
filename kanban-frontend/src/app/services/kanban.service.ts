a5d08c598fac27817470a8d377725d5c993b59ed
Stephen Hanjaya <stephen.hanjaya@sonymusicpub.com>
2026-08-25 14:14:48 -0500

    removed takeUntilDestroyed as it caused runtime error
    Signed-off-by: Stephen Hanjaya <stephen.hanjaya@sonymusicpub.com>

--- kanban-frontend/src/app/services/kanban.service.ts
+++ kanban-frontend/src/app/services/kanban.service.ts
@@ -1,7 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { computed, inject, Service, signal } from '@angular/core';
 import { BoardAggregate } from '../models/kanban.model';
-import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 import { catchError, of } from 'rxjs';
 
 /* @Service() a new decorator in place of the old @Injectable. 
@@ -16,7 +15,6 @@ export class KanbanService {
     public isLoaded = computed<boolean>(() => this.boardState() !== null);
     public loadBoard(boardId: string): void {
         this.http.get<BoardAggregate>(`/api/boards?id=${boardId}`).pipe(
-            takeUntilDestroyed(), // we don't really need to unsubscribe as this is a singleton provided in root, but this is for best practice
             catchError(error => {
                 console.error('Data stream resolution failed: ', error);
 

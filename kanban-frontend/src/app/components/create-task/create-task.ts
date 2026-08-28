606300fed52c8ec22825fdc17fcfe04fb8e500df
Stephen Hanjaya <stephen.hanjaya@sonymusicpub.com>
2026-08-28 02:36:44 -0500

    npx ng g component components/create-task --inline-templat e=false
    Signed-off-by: Stephen Hanjaya <stephen.hanjaya@sonymusicpub.com>

---
+++ kanban-frontend/src/app/components/create-task/create-task.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  imports: [],
+  selector: 'app-create-task',
+  styleUrl: './create-task.css',
+  templateUrl: './create-task.html',
+})
+export class CreateTask {}

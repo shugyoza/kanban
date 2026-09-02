5c068bf7ed24098fbec84471988a6334ca4e125d
Stephen Hanjaya <stephen.hanjaya@sonymusicpub.com>
2026-09-02 09:09:42 -0500

    BE API integration
    Signed-off-by: Stephen Hanjaya <stephen.hanjaya@sonymusicpub.com>

--- kanban-backend/cmd/api/main.go
+++ kanban-backend/cmd/api/main.go
@@ -83,6 +83,7 @@ func main() {
 	http.HandleFunc("PUT /api/tasks/move", kanbanHandler.MoveTask)
 	http.HandleFunc("POST /api/tasks", kanbanHandler.HandleTask)
 	http.HandleFunc("DELETE /api/tasks", kanbanHandler.HandleTask)
+	http.HandleFunc("PATCH /api/tasks", kanbanHandler.UpdateTask)
 
 	serverPort := ":8080"
 	// Fire up the native Go local web server

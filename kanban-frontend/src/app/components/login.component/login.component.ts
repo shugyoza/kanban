b4d196a3175b0087707a647e9fd694992c00a513
Stephen Hanjaya <stephen.hanjaya@sonymusicpub.com>
2026-09-10 15:00:22 -0500

    scaffold default login.component
    Signed-off-by: Stephen Hanjaya <stephen.hanjaya@sonymusicpub.com>

---
+++ kanban-frontend/src/app/components/login.component/login.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  imports: [],
+  selector: 'app-login.component',
+  styleUrl: './login.component.css',
+  templateUrl: './login.component.html',
+})
+export class LoginComponent {}

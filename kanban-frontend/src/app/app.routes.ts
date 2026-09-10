198704c477777414265d9a57f067513261139f8d
Stephen Hanjaya <stephen.hanjaya@sonymusicpub.com>
2026-09-10 15:11:29 -0500

    updated app.routes
    Signed-off-by: Stephen Hanjaya <stephen.hanjaya@sonymusicpub.com>

--- kanban-frontend/src/app/app.routes.ts
+++ kanban-frontend/src/app/app.routes.ts
@@ -1,3 +1,23 @@
 import { Routes } from '@angular/router';
+import { authGuard } from './guards/auth.guard';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'login',
+        loadComponent: () => import('./components/login.component/login.component').then(m => m.LoginComponent)
+    },
+    {
+        path: 'board',
+        canActivate: [authGuard],
+        loadComponent: () => import('./board.component/board.component').then(m => m.BoardComponent)
+    },
+    {
+        path: '',
+        redirectTo: 'board',
+        pathMatch: 'full'
+    },
+    {
+        path: '**',
+        redirectTo: 'board'
+    }
+];

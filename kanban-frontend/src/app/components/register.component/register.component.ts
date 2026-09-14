8a71cc1f835c2d4989282040f40f3368bb333503
Stephen Hanjaya <stephen.hanjaya@sonymusicpub.com>
2026-09-14 10:59:18 -0500

    temporary register.component update
    Signed-off-by: Stephen Hanjaya <stephen.hanjaya@sonymusicpub.com>

--- kanban-frontend/src/app/components/register.component/register.component.ts
+++ kanban-frontend/src/app/components/register.component/register.component.ts
@@ -1,4 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
+import { AuthService } from '../../services/auth.service';
+import { Credentials } from '../../models/auth.model';
+import { ActivatedRoute, Router } from '@angular/router';
+import { form, minLength, required } from '@angular/forms/signals';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
+import { finalize } from 'rxjs';
 
 @Component({
   imports: [],
@@ -6,4 +12,40 @@ import { Component } from '@angular/core';
   styleUrl: './register.component.css',
   templateUrl: './register.component.html',
 })
-export class RegisterComponent {}
+export class RegisterComponent {
+  private readonly authService = inject(AuthService);
+  private readonly router = inject(Router);
+  private readonly activatedRoute = inject(ActivatedRoute);
+  private readonly token = this.activatedRoute.snapshot.queryParamMap.get('token') ?? '';
+
+  private readonly credentialsModel = signal<Required<Credentials>>({
+    username: '',
+    password: ''
+  })
+
+  protected readonly registerForm = form(this.credentialsModel, schemaPath => {
+    required(schemaPath.username, { message: 'Username is required' });
+    required(schemaPath.password, { message: 'Password is required' });
+    minLength(schemaPath.password, 8, { message: 'Password must be at least 8 characters long' })
+  });
+
+  protected readonly loading = signal<boolean>(false);
+
+  protected handleSubmit($event: Event): void {
+    $event.preventDefault();
+
+    if (this.registerForm().invalid()) {
+      console.error('invalid register form');
+
+      return;
+    }
+
+    this.loading.set(true);
+    this.authService.login(this.credentialsModel()).pipe(
+      takeUntilDestroyed(),
+      finalize(() => {
+        this.loading.set(false)
+      })
+    ).subscribe();
+  }
+}

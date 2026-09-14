f8f4b5c5aadf7a0089882a96fd3aee6f5929b5d0
Stephen Hanjaya <stephen.hanjaya@sonymusicpub.com>
2026-09-14 09:20:36 -0500

    npx ng g c register.component
    Signed-off-by: Stephen Hanjaya <stephen.hanjaya@sonymusicpub.com>

---
+++ kanban-frontend/src/app/components/register.component/register.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  imports: [],
+  selector: 'app-register.component',
+  styleUrl: './register.component.css',
+  templateUrl: './register.component.html',
+})
+export class RegisterComponent {}

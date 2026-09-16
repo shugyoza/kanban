87c9a4ace2e4a39033e4b7d8f46197d2f82cc01b
Stephen Hanjaya <stephen.hanjaya@sonymusicpub.com>
2026-09-16 09:18:16 -0500

    updated auth.guard
    Signed-off-by: Stephen Hanjaya <stephen.hanjaya@sonymusicpub.com>

--- kanban-frontend/src/app/guards/auth.guard.ts
+++ kanban-frontend/src/app/guards/auth.guard.ts
@@ -1,18 +1,15 @@
 import { inject } from '@angular/core';
 import { CanActivateFn, Router } from '@angular/router';
 import { AuthService } from '../services/auth.service';
-import { map, tap } from 'rxjs';
 
 export const authGuard: CanActivateFn = () => {
   const authService = inject(AuthService);
   const router = inject(Router);
+  const isAuthenticated = authService.isAuthenticated();
 
-  return authService.getCurrentUser().pipe(
-    map(user => !!user),
-    tap(isLoggedIn => {
-      if (!isLoggedIn) {
-        router.navigate(['/', 'login']);
-      }
-    })
-  )
+  if (!isAuthenticated) {
+    router.navigate(['/', 'login']);
+  }
+
+  return isAuthenticated;
 };

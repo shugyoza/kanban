a9268ff609af9272377674a78db08bc7a25786ec
Stephen Hanjaya <stephen.hanjaya@sonymusicpub.com>
2026-09-16 10:44:12 -0500

    added logout feature
    Signed-off-by: Stephen Hanjaya <stephen.hanjaya@sonymusicpub.com>

--- kanban-frontend/src/app/app.ts
+++ kanban-frontend/src/app/app.ts
@@ -1,6 +1,7 @@
 import { Component, computed, inject, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { AuthService } from './services/auth.service';
+import { User } from './models/auth.model';
 
 @Component({
   imports: [RouterOutlet],
@@ -11,5 +12,9 @@ import { AuthService } from './services/auth.service';
 export class App {
   private readonly authService = inject(AuthService);
   protected readonly title = signal('kanban-frontend');
-  protected readonly user = computed(() => this.authService.currentUser());
+  protected readonly user = computed<User | null | undefined>(() => this.authService.currentUser());
+
+  public logout(): void {
+    this.authService.logout();
+  }
 }

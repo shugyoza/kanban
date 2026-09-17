aa06495c1d03703a9de02934c1f0f8163ece4e17
Stephen Hanjaya <stephen.hanjaya@sonymusicpub.com>
2026-09-17 16:24:03 -0500

    updated angular guard for validating invite token
    Signed-off-by: Stephen Hanjaya <stephen.hanjaya@sonymusicpub.com>

--- kanban-frontend/src/app/guards/invite.guard.ts
+++ kanban-frontend/src/app/guards/invite.guard.ts
@@ -1,12 +1,33 @@
 import { inject } from '@angular/core';
 import { CanActivateFn, Router } from '@angular/router';
+import { InvitationService } from '../services/invitation.service';
+import { catchError, map, of } from 'rxjs';
 
 export const inviteGuard: CanActivateFn = (route) => {
   const router = inject(Router);
+  const inviteService = inject(InvitationService);
+
   const token = route.queryParamMap.get('token');
 
   if (token && token.trim().length > 0) {
-    return true;
+    return inviteService.validateInvitationToken(token).pipe(
+      map(response => {
+        const isValidToken = response.valid;
+
+        if (!isValidToken) {
+          router.navigate(['/', 'login']);
+        }
+
+        return isValidToken;
+      }),
+      catchError(error => {
+        console.error(error);
+
+        router.navigate(['/', 'login']);
+
+        return of(false)
+      })
+    );
   }
 
   router.navigate(['/', 'login']);
